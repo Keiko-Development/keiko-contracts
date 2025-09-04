04a32653467ad166d388949bafb1d50ca666adba
oscharko <o.scharkowski@oscharko.de>
2025-09-04 17:19:11 +0200

    Add Jest configuration for unit test setup and coverage reports

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: [
+    '**/tests/**/*.test.js',
+    '**/__tests__/**/*.js',
+    '**/?(*.)+(spec|test).js'
+  ],
+  collectCoverageFrom: [
+    'server.js',
+    '!node_modules/**',
+    '!coverage/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: [
+    'text',
+    'lcov',
+    'html'
+  ],
+  testTimeout: 10000,
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+  verbose: true
+};
